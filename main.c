878d5754dc87355f2acb5036293f0d2ce6c906c6
Pablo Turati <pablo.turati@gmail.com>
2021-01-10 08:18:58 -0500

    feat(main.c): adds title and year

--- main.c
+++ main.c
@@ -6,20 +6,88 @@
 struct movie
 {
   char *title;
-  int year;
+  short year;
   // char *languages[MAX_NUMBER_OF_LANGUAGES];
-  float rating;
+  // float rating;
   struct movie *next;
 };
 
+struct movie *createMovieList(char *);
+struct movie *createMovie(char *);
+
+struct movie *createMovieList(char *filePath)
+{
+  // Opens file for reading
+  FILE *movieFile = fopen(filePath, "r");
+  char *currLine = NULL;
+  size_t len = 0;
+  ssize_t nread;
+  char *token;
+
+  struct movie *head = NULL;
+  struct movie *tail = NULL;
+
+  // Parse each line to linkedlist objects
+  while ((nread = getline(&currLine, &len, movieFile)) != -1)
+  {
+    struct movie *newNode = createMovie(currLine);
+    if (head == NULL)
+    {
+      head = newNode;
+      tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  }
+  free(currLine);
+  return head;
+}
+
+struct movie *createMovie(char *currLine)
+{
+  struct movie *currMovie = malloc(sizeof(struct movie));
+
+  // For use with strtok_r
+  char *rest;
+
+  // Title
+  char *token = strtok_r(currLine, ",", &rest);
+  currMovie->title = calloc(strlen(token) + 1, sizeof(char));
+  strcpy(currMovie->title, token);
+
+  // Year
+  token = strtok_r(NULL, ",", &rest);
+  currMovie->year = atoi(token);
+
+  // Set next movie ptr
+  currMovie->next = NULL;
+
+  return currMovie;
+}
+
+void printList(struct movie *list)
+{
+  printf("Printing");
+  while (list != NULL)
+  {
+    printf("Title:  %s", list->title);
+    printf("Year:  %d", list->year);
+    list = list->next;
+  }
+}
+
 int main(int argc, char *argv[])
 {
-  // if (argc < 2)
-  // {
-  //   printf("Missing file to process");
-  //   return EXIT_FAILURE;
-  // }
+  if (argc < 2)
+  {
+    printf("Missing file to process");
+    return EXIT_FAILURE;
+  }
 
-  printf("%d", MAX_NUMBER_OF_LANGUAGES);
-  return 0;
+  struct movie *movieList = createMovieList(argv[1]);
+  printList(movieList);
+  return EXIT_SUCCESS;
 }
--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
-rm -f movies constants.o lib_movies.a
-gcc --std=c99 -c constants.c
-ar -r lib_movies.a constants.o
-gcc --std=c99 -o movies main.c lib_movies.a
+# Conditional cleanup space from previously compiled files
 rm -f movies constants.o lib_movies.a
 
-# gcc --std=c99 -o test main.c constants.c
+# Creates
+gcc --std=c99 -c constants.c
+
+# Creates external file archive
+ar -r lib_movies.a constants.o
+
+# Compiles main
+gcc --std=c99 -o movies main.c lib_movies.a
+
+# Removes temporary files
+rm -f constants.o lib_movies.a
+
+# REMVOE gcc --std=c99 -o test main.c constants.c
+./movies movies_sample_1.csv
